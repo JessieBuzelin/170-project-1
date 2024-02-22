104eba7406afa6dd2a41aa680a821eec1bcab954
SAE1\1031854 <1031854@student.sae.edu.au>
2024-02-22 11:02:36 +1100

    enemy health started

--- Assets/EnemyHealth.cs
+++ Assets/EnemyHealth.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class EnemyHealth : MonoBehaviour
 {
+    private int enemyHealth = 100;
+        public int enemyAgility = 10;
+    public int enemyHaste = 15;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,6 @@ public class EnemyHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("haste is " + enemyHaste);
     }
 }
